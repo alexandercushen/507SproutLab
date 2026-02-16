4b6fd6c382410fd49a0a0baf5ecdb4634e842514
alexandercushen <atcushen@umich.edu>
2026-02-15 19:14:09 -0500

    Fixed the multi-page system so now we have nice card tabs to navigate

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def plant_log():
     return render_template('plant_log.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 '''
 from flask import Flask, render_template, request
